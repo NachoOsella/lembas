c5aaa3f8e478335a35aaaff43baeb65d8c7ac218
NachoOsella <nachoosella03@gmail.com>
2026-05-25 20:53:09 -0300

    change password

--- backend/src/main/resources/db/migration/V10__seed_data.sql
+++ backend/src/main/resources/db/migration/V10__seed_data.sql
@@ -29,7 +29,7 @@ INSERT INTO users (branch_id, email, password_hash, first_name, last_name, role,
 VALUES (
     (SELECT id FROM branches WHERE name = 'Centro'),
     'admin@lembas.com',
-    '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy',
+    '$2a$10$4/FkImI6zRwqfW4s7Plg9.mW2ELmk2iqvl2NTRD21V4gi75ltxPXG',
     'Admin',
     'Lembas',
     'ADMIN',
